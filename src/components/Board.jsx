8b54b3acef12332b9427e46e6aa1feb689fd853e
Wilson-Chu <wilson1.chu@gmail.com>
2023-03-29 02:08:25 -0400

    Initial setup

---
+++ src/components/Board.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Board() {
+  return <div>Base board here...</div>;
+}
+
+export default Board;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer() {
+  return <div>Made by Team ChinguV43T02T24 Team Name TBD!</div>;
+}
+
+export default Footer;
