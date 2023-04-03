984720c29d6b515804f01c203a04075a9cf43073
Pipexlul <felipgg@gmail.com>
2023-04-03 03:04:32 -0400

    Remove flowbite and flowbite-react dependencies, main reason is that flowbite documentation was not finished yet and considering our time constraint we need something we can implement and utilize with ease.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-    require("flowbite/plugin"),
-  ],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
