54f1b5fcf8e337046cb82d2611e0521a194b932e
wilson <wilson1.chu@gmail.com>
2023-04-10 04:47:59 -0400

    removed some comments

--- src/components/DragAndDrop.jsx
+++ src/components/DragAndDrop.jsx
@@ -1,34 +1,3 @@
-// import React, { useState } from "react";
-// import Draggable from "react-draggable";
-
-// function DragAndDrop({ items }) {
-//   const [list, setList] = useState(items);
-
-//   const handleDrag = (index, newPosition) => {
-//     setList((prevList) => {
-//       const newList = [...prevList];
-//       newList[index].position = newPosition;
-//       return newList;
-//     });
-//   };
-
-//   return (
-//     <div>
-//       {list.map((item, index) => (
-//         <Draggable
-//           key={index}
-//           defaultPosition={item.position}
-//           onStop={(event, data) => handleDrag(index, data.x, data.y)}
-//         >
-//           <div>{item.content}</div>
-//         </Draggable>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default DragAndDrop;
-
 import React, { useState, useEffect, useRef } from "react";
 import Draggable from "react-draggable";
 
@@ -46,7 +15,7 @@ function DragAndDrop() {
     setPositions(existingDivPositions);
     setHasLoaded(true);
     console.log(existingDivPositions);
-    console.log("has loaded");
+    console.log(`has loaded`);
   }, []);
 
   function handleStop(e, data) {
